fd04541ead1178475af727d7513156a0f8e5ee9c
Jake Girhiny <jegirhiny@gmail.com>
2023-09-21 23:46:12 -0400

    11.7 complete

--- 11.7/app.js
+++ 11.7/app.js
@@ -1,7 +1,30 @@
 const hasDuplicate = (array) => {
-    
+    let set = new Set();
+
+    for(const value of array) {
+        if(set.has(value)) {
+            return true;
+        }
+
+        set.add(value);
+    }
+
+    return false;
 };
 
 const vowelCount = (string) => {
-    
+    const vowels = 'aeiou';
+    let map = new Map();
+
+    for(let char of string.toLowerCase()) {
+        if(vowels.includes(char)) {
+            if(map.has(char)) {
+                map.set(char, map.get(char) + 1);
+            } else {
+                map.set(char, 1);
+            }
+        }
+    }
+
+    return map;
 };
