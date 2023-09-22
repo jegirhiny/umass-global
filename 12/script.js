26edc0e889e76cb891ca176b87b719b72423738e
Jake Girhiny <jegirhiny@gmail.com>
2023-09-22 16:53:36 -0400

    12 comments added

--- 12/script.js
+++ 12/script.js
@@ -3,6 +3,11 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+/**
+ * Searches for substrings in an array and returns the matching elements.
+ * @param {string} str - The substring to search for.
+ * @returns {Array} Collection containing matching elements.
+ */
 function search(str) {
 	if(str.length === 0) {
 		return [];
@@ -19,10 +24,18 @@ function search(str) {
 	return results;
 }
 
+/**
+ * Handles the search input event.
+ * @param {Event} e - The search input event.
+ */
 function searchHandler(e) {
 	showSuggestions(search(e.target.value));
 }
 
+/**
+ * Appends search suggestions on the web page.
+ * @param {Array} results - Collection of suggestions to display.
+ */
 function showSuggestions(results) {
 	suggestions.innerHTML = '';
 
@@ -34,6 +47,10 @@ function showSuggestions(results) {
 	})
 }
 
+/**
+ * Clears all suggestions and updates the input field.
+ * @param {Event} e - The suggestion click event
+ */
 function useSuggestion(e) {
 	showSuggestions([]);
 	input.value = e.target.innerText;
