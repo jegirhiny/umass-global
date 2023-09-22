071ab3b5165e273b62bc061a1d9840c4fcd19860
Jake Girhiny <jegirhiny@gmail.com>
2023-09-22 12:50:19 -0400

    12 improved functions

--- 12/script.js
+++ 12/script.js
@@ -4,6 +4,10 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
+	if(str.length === 0) {
+		return [];
+	}
+
 	let results = [];
 
 	fruit.forEach(value => {
@@ -16,20 +20,18 @@ function search(str) {
 }
 
 function searchHandler(e) {
-	showSuggestions(search(input.value), e.value);
+	showSuggestions(search(e.target.value));
 }
 
-function showSuggestions(results, inputVal) {
+function showSuggestions(results) {
 	suggestions.innerHTML = '';
 
-	if(results.length !== fruit.length) {
-		results.forEach(value => {
-			let li = document.createElement('li');
-			li.innerText = value;
+	results.forEach(value => {
+		let li = document.createElement('li');
+		li.innerText = value;
 
-			suggestions.append(li);
-		})
-	}
+		suggestions.append(li);
+	})
 }
 
 function useSuggestion(e) {
