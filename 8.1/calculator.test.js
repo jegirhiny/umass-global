574b9a530b8940f7d462c569c7048237a500392c
Jake Girhiny <jegirhiny@gmail.com>
2023-09-24 21:18:58 -0400

    8.1 code refactor

--- 8.1/calculator.test.js
+++ 8.1/calculator.test.js
@@ -4,14 +4,11 @@
 */
 
 it("should calculate the monthly rate correctly", function () {
-  
+  expect(calculateMonthlyPayment({amount: 1, years: 1, rate: 1})).toEqual("2");
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  setupIntialValues();
-  update();
-  debugger;
   let payment = document.getElementById("monthly-payment");
   let fixedPayment = String(Number(payment).toFixed(2));
 
