1d6ec73c631e86a26602688f1addf13b4b3e86b5
Jake Girhiny <jegirhiny@gmail.com>
2023-09-26 16:54:56 -0400

    14.1-2 sorting complete

--- 14.1-2/app.js
+++ 14.1-2/app.js
@@ -1,17 +1,10 @@
 const MIN_LENGTH = 2, MIN_RATING = 0, MAX_RATING = 10;
 const movieContainer = $('#movie-container');
 
-$('#movie-form').on('submit', (e) => {
-    e.preventDefault();
-    const [title, rating] = [$('input').eq(0).val(), $('input').eq(1).val()];
-
-    if(title.length < MIN_LENGTH || rating < MIN_RATING || rating > MAX_RATING) {
-        return;
-    }
-
+const appendMovie = (title, rating) => {
     movieContainer.append(
         $('<div></div>').append(
-            $(`<h2>`, {text : `${title} ${rating}/10`})
+            $(`<h2>`, {text : `${title} ${rating}/10`}).attr('title', title).attr('rating', rating)
         ).append(
             $(`<button>`, {text : 'Remove'}).on('click', (e) => {
                 $(e.target).closest('div').remove();
@@ -21,6 +14,38 @@ $('#movie-form').on('submit', (e) => {
             'justify-content' : 'center'
         })
     );
+}
 
+const sortByRating = () => {
+    let sorted = [], movieData = movieContainer.find('h2');
+
+    for(let rating = 0; rating < 11; rating++) {
+        movieData.toArray().forEach(value => {
+            let movieRating = value.getAttribute('rating');
+
+            if(rating === +movieRating) {
+                sorted.push(value);
+            }
+        })
+    }
+
+    movieContainer.empty();
+    
+    sorted.forEach((movie) => {
+        appendMovie(movie.getAttribute('title'), movie.getAttribute('rating'));
+    })
+}
+
+$('#movie-form').on('submit', (e) => {
+    e.preventDefault();
+    const [title, rating] = [$('input').eq(0).val(), $('input').eq(1).val()];
+
+    if(title.length < MIN_LENGTH || rating < MIN_RATING || rating > MAX_RATING) {
+        return;
+    }
+
+    appendMovie(title, rating);
     $('#movie-form').trigger('reset');
 })
+
+$('#sort-button').on('click', () => sortByRating())
