6480a10d05710d6f1f65e362d6d1620cc4ca713a
Jake Girhiny <jegirhiny@gmail.com>
2023-09-28 21:53:52 -0400

    15.3 functionality complete

--- 15.3/app.js
+++ 15.3/app.js
@@ -1,1 +1,36 @@
-console.log("Let's get this party started!");
+const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+$('#search-form').on('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = $('#search-term').val();
+    reqGif(searchTerm, apiKey);
+
+    $('#search-form').trigger('reset');
+})
+
+$('#remove-all').on('click', (e) => {
+    e.preventDefault();
+
+    $('video').remove();
+}) 
+
+async function reqGif(searchTerm, apiKey) {
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', {params: {q : searchTerm, api_key : apiKey}});
+    const content = res.data.data;
+
+    if(content.length === 0) {
+        console.log('Invalid entry term');
+    } else {
+        const video = content[Math.floor(Math.random() * content.length)];
+
+        $('body').append(
+            $('<video>').append(
+                $('<source>', {
+                    src: video.images.looping.mp4,
+                    type: 'video/mp4'
+                })
+            ).attr('autoplay', 'autoplay')
+        );
+    }
+}
