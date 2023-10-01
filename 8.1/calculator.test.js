6d870bf5d3c24f889e11d20d04a38c5a1daeec9e
Jake Girhiny <jegirhiny@gmail.com>
2023-09-30 20:05:06 -0400

    8.1 completed

--- 8.1/calculator.test.js
+++ 8.1/calculator.test.js
@@ -1,15 +1,10 @@
-/**
- * - postive and negative test case
- * - loading correct values to the dom (inital)
-*/
-
 it("should calculate the monthly rate correctly", function () {
-  expect(calculateMonthlyPayment({amount: 1, years: 1, rate: 1})).toEqual("2");
+  expect(calculateMonthlyPayment({ amount: 1, years: 1, rate: 1 })).toEqual("0.13");
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  let payment = document.getElementById("monthly-payment");
+  let payment = calculateMonthlyPayment({amount : 5, years : 5, rate : 5});
   let fixedPayment = String(Number(payment).toFixed(2));
 
   expect(payment.length === fixedPayment.length).toBe(true);
