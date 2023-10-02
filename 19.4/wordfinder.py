b306fe8d6a8e97dd7f705b07fd990e1b00a0b5f0
Jake Girhiny <jegirhiny@gmail.com>
2023-10-02 15:59:40 -0400

    19.4 complete

--- 19.4/wordfinder.py
+++ 19.4/wordfinder.py
@@ -1,4 +1,12 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
+
 class WordFinder:
-    ...
+    def __init__(self, path):
+        self.file = open(path);
+        self.words = self.file.read().split('\n')
+        self.words_length = len(self.words)
+
+    def random(self):
+        print(self.words[int(random.random() * self.words_length - 1)])
