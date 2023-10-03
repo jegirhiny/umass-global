d2ca7eb15a2aeb3ed589de6d7019d848c106cbe9
Jake Girhiny <jegirhiny@gmail.com>
2023-10-02 20:00:13 -0400

    20.1 exercise complete

--- 20.1/calc/app.py
+++ 20.1/calc/app.py
@@ -1,1 +1,20 @@
-# Put your app in here.import 
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/math/<math_type>')
+def calc(math_type):
+    a = float(request.args['a'])
+    b = float(request.args['b'])
+
+    if math_type == 'add':
+        return str(add(a, b))
+    elif math_type == 'sub':
+        return str(sub(a, b))
+    elif math_type == 'mult':
+        return str(mult(a, b))
+    elif math_type == 'div':
+        return str(div(a, b))
+    
+    return 'Unknown Math Type'
