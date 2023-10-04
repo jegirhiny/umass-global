a991cf33d6886dd1d26bc344094ccf67830675c0
Jake Girhiny <jegirhiny@gmail.com>
2023-10-04 13:13:26 -0400

    20.2 complete, finish further study

--- 20.2/app.py
+++ 20.2/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, request, render_template
+from stories import *
 
 app = Flask(__name__)
 
+story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
 @app.route('/')
 def landing_form():
-    return render_template('form.html')
+    return render_template('form.html', prompts = story.prompts)
+
+@app.route('/story', methods=['GET'])
+def story_page():
+    return render_template('story.html', story = story.generate(request.args))
