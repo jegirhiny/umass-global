4a8a913bb344980c4d35c4dd9fb1b053d738d2b6
Jake Girhiny <jegirhiny@gmail.com>
2023-10-09 19:18:50 -0400

    23.2 queries_playstore.sql half complete

--- 23.2/queries_playstore.sql
+++ 23.2/queries_playstore.sql
@@ -1,20 +1,20 @@
 -- 1. Find the app with an ID of ***1880*** --
-
+SELECT app_name FROM analytics WHERE id = 1800;
 
 -- 2. Find the ID and app name for all apps that were last updated on August 01, 2018. --
-
+SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
 
 -- 3. Count the number of apps in each category, e.g. “Family | 1972”. --
-
+SELECT category, COUNT(category) AS num_apps FROM analytics GROUP BY category;
 
 -- 4. Find the top 5 most-reviewed apps and the number of reviews for each. --
-
+SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
 
 -- 5. Find the app that has the most reviews with a rating greater than equal to 4.8. --
-
+SELECT app_name, rating, reviews FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
 
 -- 6. Find the average rating for each category ordered by the highest rated to lowest rated. --
-
+SELECT category, AVG(rating) AS avg_rating FROM analytics GROUP BY category ORDER BY avg_rating DESC;
 
 -- 7. Find the name, price, and rating of the most expensive app with a rating that’s less than 3. --
 
