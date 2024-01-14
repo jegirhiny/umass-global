adc2d4f79dd62c9e4353b1f4a8529c1de2b72d09
Jake Girhiny <jegirhiny@gmail.com>
2024-01-14 01:38:54 -0500

    refactored error messages in exercise 20, good for submit

--- 20/app.py
+++ 20/app.py
@@ -2,31 +2,41 @@ import requests
 from flask import Flask, request, render_template
 
 ACCESS_KEY = "ed6417766bff0bc28cb45ab9fadae011"
+BASE_API = "http://api.exchangerate.host"
+LIST_ENDPOINT = "/list"
+CONVERT_ENDPOINT = "/convert"
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def landing():
     return render_template('form.html')
 
 
-@app.route('/result')   
+@app.route('/result', methods=['GET'])
 def result():
     from_currency = request.args['from_currency'].upper()
     to_currency = request.args['to_currency'].upper()
     amount = float(request.args['amount'])
 
-    currencyRes = requests.get(f"http://api.exchangerate.host/list?access_key={ACCESS_KEY}")
-    currencyList = currencyRes.json()["currencies"]
+    currency_res = requests.get(f"{BASE_API}{LIST_ENDPOINT}?access_key={ACCESS_KEY}")
+    currency_list = currency_res.json()["currencies"]
 
-    if(from_currency not in currencyList and to_currency not in currencyList) :
-        return render_template('form.html', from_currency = from_currency, to_currency = to_currency, amount = float(amount), errors = [f"Invalid Currency: {from_currency}", f"Invalid Currency: {to_currency}"])
-    elif (from_currency not in currencyList) :
-        return render_template('form.html', from_currency = from_currency, to_currency = to_currency, amount = float(amount), errors = [f"Invalid Currency: {from_currency}"])
-    elif (to_currency not in currencyList) :
-        return render_template('form.html', from_currency = from_currency, to_currency = to_currency, amount = float(amount), errors = [f"Invalid Currency: {to_currency}"])
+    errors = []
 
-    convertRes = requests.get(f"http://api.exchangerate.host/convert?access_key={ACCESS_KEY}&from={from_currency}&to={to_currency}&amount={amount}")
-    result = convertRes.json()["result"]
+    if (from_currency not in currency_list):
+        errors.append(f"Invalid Currency: {from_currency}")
+    
+    if (to_currency not in currency_list):
+        errors.append(f"Invalid Currency: {to_currency}")
+
+    if (amount < 0):
+        errors.append(f"Invalid Amount: {amount}")
+
+    if len(errors) > 0:
+        return render_template('form.html', from_currency = from_currency, to_currency = to_currency, amount = float(amount), errors = errors)
+
+    convert_res = requests.get(f"{BASE_API}{CONVERT_ENDPOINT}?access_key={ACCESS_KEY}&from={from_currency}&to={to_currency}&amount={amount}")
+    result = convert_res.json()["result"]
 
     return render_template('result.html', result = round(result, 2))
